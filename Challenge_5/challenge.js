7375a2f0f2b45ac058f88432c51190ecefa198e2
Thuy Doan <clearlyTHUYDOAN@Thuys-MBP.cogeco.net>
2016-09-13 17:17:23 -0400

    writing javascript complete

--- Challenge_5/challenge.js
+++ Challenge_5/challenge.js
@@ -25,7 +25,15 @@ console.log(a && b && c);
 
 //Exercise 5
 
-
+answer1 = 10 - 3 * 5/4 + 202 % 3
+answer2 = 10 - (3 * (5/4) + (202 % 3))
+console.log(answer1);
+console.log(answer2);
 
 //Exercise 6
 
+String(12);
+parseInt("113")
+parseFloat(1.12356)
+x = parseInt("Hello World")
+console.log(x);
