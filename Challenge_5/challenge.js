04ac07aa010a58c5517ca4ddd6fa479c2b7ff9eb
Thuy Doan <clearlyTHUYDOAN@Thuys-MBP.cogeco.net>
2016-09-13 14:42:35 -0400

    starting javascript challenge

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,25 @@
+//Exercise 1
+
+var name = "Thuy";
+console.log("Hello " + name);
+
+//Exercise 2
+
+var x = 1125;
+var answer = x % 2;
+console.log(answer);
+
+//Exercise 3
+
+
+
+//Exercise 4
+
+
+
+//Exercise 5
+
+
+
+//Exercise 6
+
