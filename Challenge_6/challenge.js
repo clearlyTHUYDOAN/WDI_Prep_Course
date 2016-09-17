35d75114a7e5e4b923bb0942bdc320f183aabbcc
Thuy Doan <clearlyTHUYDOAN@Thuys-MBP.cogeco.net>
2016-09-17 16:17:30 -0400

    exercises 1-3

--- Challenge_6/challenge.js
+++ Challenge_6/challenge.js
@@ -1,12 +1,12 @@
 /*
-Exercise #1
+Exercise #1 - complete
 Initialize an empty array below in the variable numbers.
 */
 var numbers = [];
 
 
 /*
-Exercise #2
+Exercise #2 - complete
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
@@ -26,18 +26,20 @@ for(var i=0;i<10;i++){
 }
 
 /*
-Exercise #3
+Exercise #3 - complete, debug works in VSC but can't run
 Test that you used the array push method correctly by printing the sum of the values at position 3 and 6 of the array (use the console.log() function to print to the console). 
 The outputted value should be 9.
 */
 
-
+var sum = numbers[3] + numbers[6]
+console.log(sum)
 
 
 /*
 Exercise #4
 Use the console.log() function and the array .pop() to add all the values in the array.
 */
+//If .pop() removes the last value in the array, how do we add all the numbers?
 
 var sum = 0;
 /* remove this line to uncomment the code block
