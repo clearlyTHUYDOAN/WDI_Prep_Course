0f3c395d2b5661bd24624feacb317fe234508029
Thuy Doan <clearlyTHUYDOAN@Thuys-MBP.cogeco.net>
2016-09-17 17:22:04 -0400

    update

--- Challenge_6/challenge.js
+++ Challenge_6/challenge.js
@@ -6,7 +6,7 @@ var numbers = [];
 
 
 /*
-Exercise #2 - (complete, didn't understand the purpose of the foor loop)
+Exercise #2 - (complete, didn't understand the purpose of the for loop)
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
